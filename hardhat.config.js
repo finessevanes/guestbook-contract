8fd66b9a0f37e6220d240e17706292d9db7dca35
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2023-03-02 07:22:20 -0700

    add sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config();
+require("dotenv").config();
 
-const { ALCHEMY_API_KEY, GOERLI_PRIVATE_KEY } = process.env;
+const { POKT_API_KEY_GOERLI, GOERLI_PRIVATE_KEY, ALCHEMY_API_KEY_SEPOLIA } = process.env;
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
     hardhat: { chainId: 1337 },
     localhost: { chainId: 1337 },
-    goerli: { 
-      url:`https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`0x${GOERLI_PRIVATE_KEY}`]
-     },
-  }
+    goerli: {
+      url: `https://eth-goerli.gateway.pokt.network/v1/${POKT_API_KEY_GOERLI}`,
+      accounts: [`0x${GOERLI_PRIVATE_KEY}`],
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY_SEPOLIA}`,
+      accounts: [`0x${GOERLI_PRIVATE_KEY}`],
+    },
+  },
 };
